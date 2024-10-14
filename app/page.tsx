2d03bbf0a6783f3dd893356b3b77d9a38adb74ab
maanitshah25 <114627734+maanitshah25@users.noreply.github.com>
2024-10-15 01:18:45 +0400

    Updated experience entries and cleaned up UI

--- app/page.tsx
+++ app/page.tsx
@@ -13,9 +13,7 @@ import Certificate1 from '../public/certificate1-img.jpg'
 import StudySpace from '../public/StudySpace4.png'
 import Marketplace from '../public/Marketplace.png'
 import GDSC from '../public/GDSC-img.jpeg'
-import DC from '../public/DC-img.jpeg'
 import FOMO from '../public/Fomo1.jpg'
-import Cognizant from '../public/Cognizant-img.jpeg'
 import Coursera1 from '../public/Coursera1-img.png'
 import Coursera2 from '../public/Coursera2-img.png'
 import Coursera3 from '../public/Coursera3-img.png'
@@ -23,6 +21,8 @@ import Coursera4 from '../public/Coursera4-img.png'
 import Coursera5 from '../public/Coursera5-img.png'
 import Coursera6 from '../public/Coursera6-img.png'
 import Coursera7 from '../public/Coursera7-img.png'
+import AiSummit from '../public/AiSummit-img.jpeg'
+import Icarus from '../public/Icarus-img.jpeg'
 
 export default function Home() {
   return (
@@ -36,47 +36,59 @@ export default function Home() {
       <main className=" bg-black px-10">
         <section className="min-h-screen">
           <nav className="fixed top-0 left-0 w-full py-4 flex justify-between bg-black z-10 shadow-md">
-            <h1><a href="#" className="text-2xl font-serif ml-8">Maanit Himanshu Shah.</a></h1>
-            <ul className="flex space-x-16 items-center pr-28">
+            <h1>
+              <a href="#" className="text-2xl font-semibold text-white ml-8 font-serif">
+                Maanit Himanshu Shah.
+              </a>
+            </h1>
+            <ul className="flex space-x-10 items-center pr-20">
               <li><a href="#about" className="text-white text-lg">About</a></li>
               <li><a href="#Projects" className="text-white text-lg">Projects</a></li>
               <li><a href="#Experience" className="text-white text-lg">Experience</a></li>
               <li><a href="#Contact" className="text-white text-lg">Contact</a></li>
             </ul>
             <div className="mr-8 mt-1">
-              <a className="bg-gradient-to-r from-slate-900 to-black text-white px-4 py-2 rounded-md" href="https://drive.google.com/file/d/16mz9IDCP5v0SJBKCfkcf7NQWqDhV6MT7/view?usp=sharing">Resume -{">"}</a>
+              <button className="flex items-center bg-transparent border border-white text-white px-4 py-2 rounded-full">
+                <a href="https://drive.google.com/file/d/16mz9IDCP5v0SJBKCfkcf7NQWqDhV6MT7/view?usp=sharing">View Resume →</a>
+              </button>
             </div>
           </nav>
-          <div className="text-center p-10">
-            <div className="relative mx-auto w-80 h-80 mt-16">
-              <Image src={Myself} alt={""} className="rounded-full"/>
+          <div className="text-center p-10 mt-20">
+            <div className="relative mx-auto w-64 h-64 mt-16 shadow-lg hover:shadow-xl transition duration-300 ease-in-out">
+              <Image src={Myself} alt="" className="rounded-full" />
             </div>
-            <h2 className="text-5xl py-2 text-white font-serif">Hello, I am Maanit</h2>
-            <h3 className="text-2xl py-0 text-white font-serif">Developer and Designer</h3>
-            <p className="text-lg py-4 pl-80 pr-80 leading-8 text-white font-serif">
-            A Computer Science and Technology Management student at University of California, Davis, looking for an opportunity in 
-            a dynamic organization to gain a better insight into the real-world applications of Computer Science, Business, and 
-            Marketing.
+            <h2 className="text-4xl py-2 text-white font-semibold font-serif">
+              Hello, I am Maanit
+            </h2>
+            <h3 className="text-xl text-orange-500 font-medium font-serif">
+              Developer and Designer
+            </h3>
+            <p className="text-lg py-4 leading-8 text-gray-300 max-w-3xl mx-auto font-serif">
+              A Computer Science and Technology Management student at University of California, Davis, looking 
+              for an opportunity in a dynamic organization to gain a better insight into the real-world 
+              applications of Computer Science, Business, and Marketing.
             </p>
           </div>
         </section>
 
-        <section id="about">
-          <div className="bg-gradient-to-r from-slate-950 to-black p-6 rounded-lg shadow-lg mb-6 font-serif">
-            <h3 className="text-4xl py-2 text-white font-serif font-semibold">About Me</h3>
-            <p className="text-lg py-3 leading-8 text-white font-serif">
+        <section id="about" className="py-6">
+        <div className="bg-gray-950 text-white p-8 rounded-lg shadow-lg">
+            <h3 className="text-4xl mb-4 font-bold text-white font-serif">
+              About Me
+            </h3>
+            <p className="text-lg mb-6 leading-8 text-white font-serif">
             I am a sophomore Computer Science student at the University of California, Davis, with a minor in 
             Tech Management. Passionate about leveraging technology to solve real-world problems, I have 
             developed a versatile skill set that includes proficiency in Python, C++, JavaScript, and frameworks
             such as React Native and Node.js.
             </p>
-            <p className="text-lg py-3 leading-8 text-white font-serif">
+            <p className="text-lg mb-6 leading-8 text-white font-serif">
             My journey in tech began with UC Davis coding clubs and hands-on projects, where I gained valuable experience 
             in software development and database management. As the Technical Lead for the Google Developer 
             Student Club, I have honed my leadership skills while collaborating on innovative solutions. I am 
             eager to continue expanding my expertise and contribute to impactful projects.
             </p>
-            <p className="text-lg py-3 leading-8 text-white font-serif">
+            <p className="text-lg mb-6 leading-8 text-white font-serif">
             Outside of work, you can often find me playing sports such as; Soccer, Cricket, Squash or Volleyball. 
             I&apos;m a massive music fanatic and have songs palying on my headphones throughtout the day. 
             I love going to the gym and hanging out with friends, 
@@ -111,11 +123,11 @@ export default function Home() {
           </div>
         </section>
 
-        <section id="Projects">
-          <div>
-            <h3 className="text-4xl py-2 mt-14 text-white font-serif font-semibold">Projects</h3>
-          </div>
-          <div className="bg-gradient-to-r from-slate-950 to-black flex items-center p-6 rounded-lg shadow-lg mb-6">
+        <section id="Projects" className="py-6">
+          <h3 className="text-4xl mb-4 font-bold font-serif text-white">
+            Projects
+          </h3>
+          <div className="flex items-center bg-transparent border border-white text-white p-6 rounded-lg shadow-lg mb-6 font-serif">
             <div className="flex-1 text-white">
               <h2 className="text-3xl font-serif font-semibold mb-2">StudySpace</h2>
               <h3 className="text-lg font-serif mb-6 text-orange-500">Web Development</h3>
@@ -133,7 +145,7 @@ export default function Home() {
             </div>
           </div>
           <br></br>
-          <div className="bg-gradient-to-r from-black to-slate-950 flex items-center p-6 rounded-lg shadow-lg mb-6">
+          <div className="flex items-center bg-transparent border border-white text-white p-6 rounded-lg shadow-lg mb-6 font-serif">
             <div className="flex-col w-auto">
               <Image src={FOMO} alt="" className="w-full max-h-96 object-cover rounded-xl" />
             </div>            
@@ -151,7 +163,7 @@ export default function Home() {
             </div>
           </div>
           <br></br>
-          <div className="bg-gradient-to-r from-slate-950 to-black flex items-center p-6 rounded-lg shadow-lg mb-6">
+          <div className="flex items-center bg-transparent border border-white text-white p-6 rounded-lg shadow-lg mb-6 font-serif">
             <div className="flex-1 text-white">
               <h2 className="text-3xl font-serif font-semibold mb-2">Aggie Marketplace</h2>
               <h3 className="text-lg font-serif mb-6 text-orange-500">App Development</h3>
@@ -178,10 +190,76 @@ export default function Home() {
             keep up with my career journey!
           </p>
           <div className="flex mb-8 space-x-4">
-            <a className="bg-gradient-to-r from-slate-900 to-black text-white px-4 py-2 rounded-md" href="http://www.linkedin.com/in/maanit-shah">LinkedIn -{">"}</a>
-            <a className="bg-gradient-to-r from-slate-900 to-black text-white px-4 py-2 rounded-md" href="https://drive.google.com/file/d/16mz9IDCP5v0SJBKCfkcf7NQWqDhV6MT7/view?usp=sharing">View Resume -{">"}</a>
+            <button className="flex items-center bg-transparent border border-white text-white px-4 py-2 rounded-full mt-4">
+              <a href="http://www.linkedin.com/in/maanit-shah">LinkedIn →</a>
+            </button>
+            <button className="flex items-center bg-transparent border border-white text-white px-4 py-2 rounded-full mt-4">
+              <a href="https://drive.google.com/file/d/16mz9IDCP5v0SJBKCfkcf7NQWqDhV6MT7/view?usp=sharing">View Resume →</a>
+            </button>
           </div>
-          <div className="bg-gradient-to-r from-slate-950 to-black p-6 rounded-lg shadow-lg mb-6 font-serif">
+          <div className="bg-transparent border border-white text-white p-6 rounded-lg shadow-lg mb-6 font-serif">
+            <div className="flex items-center mb-4">
+              <Image src={Icarus} alt="" className="h-16 w-16 mr-4 rounded-full" />
+              <div>
+                <h3 className="text-3xl font-bold">Icarus Development</h3>
+                <p className="text-xl text-orange-500">Software Developer</p>
+              </div>
+            </div>
+            <p className="mb-4 font-serif text-lg">
+              Developed a lecture summarization web application utilizing Python, JavaScript, Node.js, 
+              TypeScript, and Amazon Web Services to streamline access to educational content and improve user 
+              efficiency. Additionally, I created immersive audio for the game "The Come Up" using FL Studio, 
+              enhancing the overall auditory experience. Through collaboration with a team of over 30 members, 
+              I strengthened my multitasking abilities and teamwork skills by fostering effective communication 
+              and project management.
+            </p>
+            <div className="flex items-center text-sm text-white">
+              <div className="flex items-center mr-6">
+                <span className="material-icons-outlined"><BsBagFill/></span>
+                <span className="ml-2">Internship</span>
+              </div>
+              <div className="flex items-center mr-6">
+                <span className="material-icons-outlined"><BsFillPinFill/></span>
+                <span className="ml-2">Davis, CA</span>
+              </div>
+              <div className="flex items-center">
+                <span className="material-icons-outlined"><BsCalendar2/></span>
+                <span className="ml-2">August 2024 - Present</span>
+              </div>
+            </div>
+          </div>
+          <div className="bg-transparent border border-white text-white p-6 rounded-lg shadow-lg mb-6 font-serif">
+            <div className="flex items-center mb-4">
+              <Image src={AiSummit} alt="" className="h-16 w-16 mr-4 rounded-full" />
+              <div>
+                <h3 className="text-3xl font-bold">Ai Summit</h3>
+                <p className="text-xl text-orange-500">Tech Outreach Ananlyst</p>
+              </div>
+            </div>
+            <p className="mb-4 font-serif text-lg">
+              Enhanced speaker outreach for the AI Summit by implementing a Custom GPT Model, significantly 
+              improving communication efficiency. Additionally, I automated data consolidation using Google 
+              Apps Script, which increased data accuracy and reduced processing time. My research into over 100 
+              emerging AI and robotics companies helped identify potential speakers, ensuring a seamless 
+              execution of the summit. I also optimized CRM workflows and refined targeted email strategies, 
+              leading to improved speaker engagement and response rates.
+            </p>
+            <div className="flex items-center text-sm text-white">
+              <div className="flex items-center mr-6">
+                <span className="material-icons-outlined"><BsBagFill/></span>
+                <span className="ml-2">Internship</span>
+              </div>
+              <div className="flex items-center mr-6">
+                <span className="material-icons-outlined"><BsFillPinFill/></span>
+                <span className="ml-2">Mountain View, CA</span>
+              </div>
+              <div className="flex items-center">
+                <span className="material-icons-outlined"><BsCalendar2/></span>
+                <span className="ml-2">July 2024 - November 2024</span>
+              </div>
+            </div>
+          </div>
+          <div className="bg-transparent border border-white text-white p-6 rounded-lg shadow-lg mb-6 font-serif">
             <div className="flex items-center mb-4">
               <Image src={GDSC} alt="" className="h-16 w-16 mr-4 rounded-full" />
               <div>
@@ -205,63 +283,7 @@ export default function Home() {
               </div>
               <div className="flex items-center">
                 <span className="material-icons-outlined"><BsCalendar2/></span>
-                <span className="ml-2">September 2022 - Present</span>
-              </div>
-            </div>
-          </div>
-          <div className="bg-gradient-to-r from-black to-slate-950 p-6 rounded-lg shadow-lg mb-6 font-serif">
-            <div className="flex items-center mb-4">
-              <Image src={DC} alt="" className="h-16 w-16 mr-4 rounded-full" />
-              <div>
-                <h3 className="text-3xl font-bold">UC Davis Student Housing and Dining Services</h3>
-                <p className="text-xl text-orange-500">Student Assistant 2</p>
-              </div>
-            </div>
-            <p className="mb-4 font-serif text-lg">
-              As a Student Assistant, I represent UC Davis with alot of pride. 
-              Managing tasks such as cashiering and provided assistance with other financial tasks as required. 
-              Carried out a variety of food service tasks such as food preparation.
-              Working alongside the shift-leads in charge and providing the best service possible.
-            </p>
-            <div className="flex items-center text-sm text-white">
-              <div className="flex items-center mr-6">
-                <span className="material-icons-outlined"><BsBagFill/></span>
-                <span className="ml-2">Part-time</span>
-              </div>
-              <div className="flex items-center mr-6">
-                <span className="material-icons-outlined"><BsFillPinFill/></span>
-                <span className="ml-2">Davis, CA</span>
-              </div>
-              <div className="flex items-center">
-                <span className="material-icons-outlined"><BsCalendar2/></span>
-                <span className="ml-2">March 2023 - June 2024</span>
-              </div>
-            </div>
-          </div>
-          <div className="bg-gradient-to-r from-slate-950 to-black p-6 rounded-lg shadow-lg mb-6 font-serif">
-            <div className="flex items-center mb-4">
-              <Image src={Cognizant} alt="" className="h-16 w-16 mr-4 rounded-full" />
-              <div>
-                <h3 className="text-3xl font-bold">Cognizant Technology</h3>
-                <p className="text-xl text-orange-500">Summer Intern</p>
-              </div>
-            </div>
-            <p className="mb-4 font-serif text-lg">
-              Developed a working database with SQL and JavaScript for the financial department. Implemented 
-              advanced Java solutions while working on the software, resulting in better efficiency.
-            </p>
-            <div className="flex items-center text-sm text-white">
-              <div className="flex items-center mr-6">
-                <span className="material-icons-outlined"><BsBagFill/></span>
-                <span className="ml-2">Part-time</span>
-              </div>
-              <div className="flex items-center mr-6">
-                <span className="material-icons-outlined"><BsFillPinFill/></span>
-                <span className="ml-2">Dubai, United Arab Emirates</span>
-              </div>
-              <div className="flex items-center">
-                <span className="material-icons-outlined"><BsCalendar2/></span>
-                <span className="ml-2">July 2021 - September 2021</span>
+                <span className="ml-2">September 2022 - June 2024</span>
               </div>
             </div>
           </div>
@@ -293,7 +315,7 @@ export default function Home() {
 
         <section id="Contact">
         <div>
-          <h3 className="text-4xl py-2 mt-5 text-white font-serif font-semibold">Contact Info</h3>
+          <h3 className="text-4xl py-2 mt-14 text-white font-serif font-semibold">Contact Info</h3>
         </div>
         <div className="text-4xl flex justify-center gap-16 py-3 text-white">
             <a href="maanithshah1@gmail.com"><AiFillMail/></a>
