eb57eb24bb1971354a3373e4426c3cde9ef4f15d
maanitshah25 <114627734+maanitshah25@users.noreply.github.com>
2025-02-10 14:13:02 +0400

    Typewriter effect

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+"use client";
+
+import React from "react";
 import Head from "next/head";
 import Image from "next/image";
-import { BsFillPinFill, BsBagFill, BsCalendar2} from "react-icons/bs";
-import { AiFillMail, AiFillLinkedin, AiFillGithub, AiFillInstagram, AiFillDiscord, AiFillTwitterCircle} from "react-icons/ai";
+import { Typewriter } from "react-simple-typewriter";
+import { BsFillPinFill, BsBagFill, BsCalendar2 } from "react-icons/bs";
+import { AiFillMail, AiFillLinkedin, AiFillGithub, AiFillInstagram, AiFillDiscord } from "react-icons/ai";
 import Myself from '../public/myself1-img.jpg'
 import Baby from '../public/baby-img.jpg'
 import Family from '../public/family-img.jpg'
@@ -59,11 +63,27 @@ export default function Home() {
             <div className="relative mx-auto w-64 h-64 mt-16 shadow-lg hover:shadow-xl transition duration-300 ease-in-out">
               <Image src={Myself} alt="" className="rounded-full" />
             </div>
-            <h2 className="text-4xl py-2 text-white font-semibold font-serif">
+            <h2 className="text-4xl py-1 text-white font-semibold font-serif">
               Hello, I am Maanit
             </h2>
             <h3 className="text-xl text-orange-500 font-medium font-serif">
-              Developer and Designer
+            <Typewriter
+              words={[
+                "Full-stack Developer",
+                "AI/ML",
+                "UI/UX",
+                "Mobile/Web",
+                "Designer",
+                "Creative Problem Solver",
+                "Leader",
+              ]}
+              loop={true}
+              cursor
+              cursorStyle="_"
+              typeSpeed={80}
+              deleteSpeed={40}
+              delaySpeed={2000}
+            />
             </h3>
             <p className="text-lg py-4 leading-8 text-gray-300 max-w-3xl mx-auto font-serif">
               A Computer Science and Technology Management student at University of California, Davis, looking 
